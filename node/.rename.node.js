fea63ef15c7e3cd710fc7d02a507d58e660e5757
createdbyjurand <createdbyjurand@gmail.com>
2019-12-23 01:01:13 +0100

    .rename.node.js 1.1.0

--- node/.rename.node.js
+++ node/.rename.node.js
@@ -1,148 +1,85 @@
-// https://stackoverflow.com/questions/53981103/wait-for-user-input-from-readline-module-node-js
-
-with callbacks
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-exports.question = function(q , cb ){
-
-    var response;
-
-    rl.setPrompt(q);
-    rl.prompt();
-
-    rl.on('line', (userInput) => {
-        response = userInput;
-        rl.close();
-    });
-
-    rl.on('close', () => {
-        return cb(response);
-    });
-};
-you call it like this
-
-var age  = arki.question("how old are you? ", resp => {
-    console.log(resp);
-});
-with promises
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-exports.question = function(q){
-
-    var response;
-
-    rl.setPrompt(q);
-    rl.prompt();
-
-    return new Promise(( resolve , reject) => {
-
-        rl.on('line', (userInput) => {
-            response = userInput;
-            rl.close();
-        });
-
-        rl.on('close', () => {
-            resolve(response);
-        });
-
-    });
-
-
-};
-you call it like this
-
-arki.question("how old are you? ").then( response => console.log(response) );
-or
-
-; ( async () => {
-    console.log(await arki.question("how old are you? "));
-})();
-
-
-
-
-
-
-
 // ( new file path, old file path );
 // fs.renameSync(`./${newFileName}`, `./${files[i]}`);
 
 const fs = require('fs');
-
-const readline = require('readline');
+// const readline = require('readline');
 
 fs.readdir('./', (readdirError, files) => {
   if (readdirError) return console.error('[ FAILED ] ' + readdirError);
 
   for (let i = 0; i < files.length; i++) {
     if (files[i] !== files[i].toLowerCase()) {
-      console.log(`[ RENAME ] (lowercase)   ${files[i]}   to   ${files[i].toLowerCase()}`);
-      // fs.renameSync(`./${files[i].toLowerCase()}`, `./${files[i]}`);
+      console.log(`[ RENAME ] (lowercase)   ${files[i]}   ->   ${files[i].toLowerCase()}`);
+      fs.renameSync(`./${files[i]}`, `./${files[i].toLowerCase()}`);
       files[i] = files[i].toLowerCase();
     }
 
     if (files[i] !== files[i].split(' ').join('.')) {
-      console.log(`[ RENAME ] (space)   ${files[i]}   to   ${files[i].split(' ').join('.')}`);
-      // fs.renameSync(`./${files[i].split(' ').join('.')}`, `./${files[i]}`);
+      console.log(`[ RENAME ] (space + 1)   ${files[i]}   ->   ${files[i].split(' ').join('.')}`);
+      fs.renameSync(`./${files[i]}`, `./${files[i].split(' ').join('.')}`);
       files[i] = files[i].split(' ').join('.');
+    }
+
+    if (i + 1 < files.length) {
+      if (files[i + 1] !== files[i + 1].toLowerCase()) {
+        console.log(`[ RENAME ] (lowercase + 1)   ${files[i + 1]}   ->   ${files[i + 1].toLowerCase()}`);
+        fs.renameSync(`./${files[i + 1]}`, `./${files[i + 1].toLowerCase()}`);
+        files[i + 1] = files[i + 1].toLowerCase();
+      }
+
+      if (files[i + 1] !== files[i + 1].split(' ').join('.')) {
+        console.log(`[ RENAME ] (space)   ${files[i + 1]}   ->   ${files[i + 1].split(' ').join('.')}`);
+        fs.renameSync(`./${files[i + 1]}`, `./${files[i + 1].split(' ').join('.')}`);
+        files[i + 1] = files[i + 1].split(' ').join('.');
+      }
     }
 
     if (files[i].match(/^.*\.s\d\de\d\d\..*\.url$/)) {
       if (!files[i].match(/^.*\.s\d\de\d\d\..*\.z\.url$/)) {
-        console.log(`[ RENAME ] (z)   ${files[i]}   to   ${files[i].slice(0, files[i].length - 4) + '.z.url'}`);
-        // fs.renameSync(`./${files[i].slice(0, files[i].length - 4) + '.z.url'}`, `./${files[i]}`);
+        console.log(`[ RENAME ] (z)   ${files[i]}   ->   ${files[i].slice(0, files[i].length - 4) + '.z.url'}`);
+        fs.renameSync(`./${files[i]}`, `./${files[i].slice(0, files[i].length - 4) + '.z.url'}`);
         files[i] = files[i].slice(0, files[i].length - 4) + '.z.url';
       }
 
       if (files[i].slice(0, files[i].search(/\.s\d\de\d\d\./)) === files[i + 1].slice(0, files[i + 1].search(/\.s\d\de\d\d\./))) {
         console.log('---------- URL RENAME START ----------');
-        console.log(files[i]);
-        console.log(files[i + 1]);
-        console.log(files[i + 1].slice(0, files[i + 1].length - 4) + '.z.url   <-   ' + files[i]);
+        console.log('┌─ ' + files[i]);
+        console.log('|  ' + files[i + 1]);
+        console.log('└> ' + files[i + 1].slice(0, files[i + 1].length - 4) + '.z.url');
         console.log('----------  URL RENAME END  ----------');
 
         // TODO: ask user Y/n to rename file
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        });
+        // const rl = readline.createInterface({
+        //   input: process.stdin,
+        //   output: process.stdout
+        // });
 
-        function readLineAsync(message) {
-          return new Promise((resolve, reject) => {
-            rl.question(message, (answer) => {
-              resolve(answer);
-            });
-          });
-        }
+        // function readLineAsync(message) {
+        //   return new Promise((resolve, reject) => {
+        //     rl.question(message, (answer) => {
+        //       resolve(answer);
+        //     });
+        //   });
+        // }
 
-        // Leverages Node.js' awesome async/await functionality
-        async function demoSynchronousPrompt(expenses) {
-          var promptInput = await readLineAsync("Give me some input >");
-          console.log("Won't be executed until promptInput is received", promptInput);
-          rl.close();
-        }
+        // // Leverages Node.js' awesome async/await functionality
+        // async function demoSynchronousPrompt(expenses) {
+        //   var promptInput = await readLineAsync("Give me some input >");
+        //   console.log("Won't be executed until promptInput is received", promptInput);
+        //   rl.close();
+        // }
 
 
 
-        const foo = event.onCall(async () => {
-          // do something
-        });
+        // const foo = event.onCall(async () => {
+        //   // do something
+        // });
 
-        const answer = async readline.question(`Rename?`, answer => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
-        });
+        // const answer = async readline.question(`Rename?`, answer => {
+        //   console.log(`Thank you for your valuable feedback: ${answer}`);
+        // });
 
-        // fs.renameSync(`./${files[i + 1].slice(0, files[i + 1].length - 4) + '.z.url'}`, `./${files[i]}`);
+        fs.renameSync(`./${files[i]}`, `./${files[i + 1].slice(0, files[i + 1].length - 4) + '.z.url'}`);
 
         const tmp = files[i + 1].slice(0, files[i + 1].length - 4) + '.z.url';
         files[i] = files[i + 1];
